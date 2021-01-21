1f3eccbc9edf0ee0793cd9d6b159d49e046e8a76
Ben Botvinick <benbotvinick@gmail.com>
2021-01-21 02:51:29 -0500

    Quick fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 window.MetaLabs = function MetaLabs(portalUrl, options) {
   fetch(`https://${portalUrl}/api/release${window.location.search}`)
-    .then((response) => {
+    .then(async (response) => {
       const $purchaseButton = document.getElementById(options.purchaseButtonId);
 
       if (response.status !== 200) {
         $purchaseButton.innerHTML = options.outOfStockText || 'Out Of Stock'
       } else {
-        const release = response.json();
+        const release = await response.json();
 
         if (release.out_of_stock) $purchaseButton.innerHTML = options.outOfStockText || 'Out Of Stock'
         else {
